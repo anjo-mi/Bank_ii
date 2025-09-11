033c4277916afe0f1a3627e09274fae95e707208
kyle <nojaim.k@gmail.com>
2025-09-11 18:41:41 -0400

    fixed env location for server and tests

--- server.js
+++ server.js
@@ -4,20 +4,20 @@ import homeRoutes from "./routes/home.js";
 import connectDB from "./config/database.js";
 
 import dotenv from "dotenv";
-dotenv.config({path: './config/.env'});
+dotenv.config();
 
 connectDB();
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.use('/', homeRoutes);
+app.use("/", homeRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('server successfully running!')
-})
+  console.log("server successfully running!");
+});
