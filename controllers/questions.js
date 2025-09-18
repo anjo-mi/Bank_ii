2ad90d02683aef19437f53553d3a0a7de86003b4
kyle <nojaim.k@gmail.com>
2025-09-17 21:58:30 -0400

    matchAll matchAny categroies to questions, all jest tests pass

--- controllers/questions.js
+++ controllers/questions.js
@@ -14,12 +14,8 @@ export default {
   },
   getQuestionById: async (req, res) => {
     try {
-      console.log({req})
       const question = await Question.findById(req.body.id);
 
-      console.log({ question });
-
-      // question successfully loaded, need to render singleQuestion.ejs with the question's info
       if (question) res.render("singleQuestion", { question });
       // if (question) return res.json(question);
       else return res.status(404).send("that question does not exist in the database!");
@@ -31,14 +27,42 @@ export default {
   getQuestionsByCats: async (req, res) => {
     try {
       const allQuestions = await Question.find();
-      const query = req.query;
-      const requested = Array.from(query).length
-        ? allQuestions.filter((question) =>
-            question.categories.some((cat) => cat in query)
-          )
-        : allQuestions;
-      console.log({ requested, query });
-      res.render("questions", { allQuestions: requested });
+      const body = req.body;
+
+      // if matchAll isnt selected, assume its match any
+      const matchAll = body.matchAll ? Boolean(+body.matchAll) : false;
+      
+      // convert any requests in the body to a Set, determine if there are any
+      const cats = Array.isArray(body.categori) ? body.categori : [body.categori];
+      const categori = new Set(cats);
+
+      if (!matchAll){
+        // if there are no categories requested, respond with all questions,
+          // otherwise extract all questions that contain ANY requested categories
+          // maybe switch to "half strict"?
+            // ???"some" -> "every" (single category will ones will pop up with exact matches)???
+        const requested = categori.size
+          ? allQuestions.filter((question) =>{
+              return question.categories.some((cat) => categori.has(cat))
+            })
+          : allQuestions;
+        // res.render("questions", { allQuestions: requested });
+        return res.json( { allQuestions: requested } );
+      }else{
+        // if user wants only questions with exact matches for requested categories
+        
+        const requested = allQuestions.filter((question) => {
+          
+          // if theres only one category requested, make it an array of 1
+          
+          // question.categories should theoretically never get out of control in terms of length
+          // O(n^2) seems <'er of two evils in comparison to being needlessly complex
+          return cats.every((cat) => question.categories.includes(cat));
+
+        });
+        // res.render("questions", { allQuestions: requested });
+        return res.json( { allQuestions: requested } );
+      }
     } catch (e) {
       console.log({ e });
     }
--- routes/questions.js
+++ routes/questions.js
@@ -3,7 +3,7 @@ import questionsController from "../controllers/questions.js";
 const router = express.Router();
 
 router.get("/", questionsController.getAllQuestions);
-router.get("/categories", questionsController.getQuestionsByCats);
+router.post("/byCategory", questionsController.getQuestionsByCats);
 router.post("/byId", questionsController.getQuestionById);
 
 export default router;
