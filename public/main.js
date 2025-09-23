98fd4a73e66593c4894075f2b17b35d3f1fa9a2d
kyle <nojaim.k@gmail.com>
2025-09-22 21:46:29 -0400

    main js ability to refine search, tests pass with playwright

---
+++ public/main.js
@@ -0,0 +1,26 @@
+// console.log(searchedQuestions);
+
+const searchedQs = Array.from(document.querySelectorAll('.question-list-item'));
+
+const searchButton = document.getElementById('search-btn');
+
+const filterSearch = () => {
+  const searchItems = document.getElementById('search-bar').value.split(' ');
+  document.getElementById('search-bar').value = '';
+  const search = searchItems.map(word => word.toLowerCase().trim()).filter(Boolean);
+  if (!search.length) return;
+  
+  const pass = searchedQs.filter(question => {
+    return search.every(item => question.textContent.toLowerCase().includes(item));
+  });
+  
+  const fail = searchedQs.filter(question => {
+    return search.some(item => !question.textContent.toLowerCase().includes(item));
+  });
+  
+  pass.forEach(question => question.style.display = 'block');
+  fail.forEach(question => question.style.display = 'none');
+}
+
+// document.addEventListener('keydown', filterSearch);
+searchButton.addEventListener('click', filterSearch);
