7e9806c3a783b7a30488260b133a646265ffc430
kyle <nojaim.k@gmail.com>
2025-11-15 21:31:28 -0500

    results pages have carousel functionality and displays

--- public/saveAnswer.js
+++ public/saveAnswer.js
@@ -2,6 +2,8 @@ const successBox = document.getElementById('success-message');
 const errorBox = document.getElementById('error-message');
 const dismisses = document.querySelectorAll('.dismiss');
 const resultCards = Array.from(document.querySelectorAll('.result-card'));
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
 
 if (dismisses) dismisses.forEach(dismissBox => {
   dismissBox.addEventListener('click', (e) => {
@@ -29,6 +31,74 @@ const handleBad = (message) => {
     errorBox.style.opacity = 100;
   }, 100)
 }
+
+const makeList = (array) => {
+  class Node {
+    constructor(val){
+      this.val = val;
+      this.next = null;
+      this.prev = null;
+    }
+  }
+  let head = null,
+      tail = null;
+  for (let i = 0 ; i < array.length ; i++){
+    const n = new Node(array[i]);
+    if (!head){
+      head = n;
+      tail = n;
+      head.next = tail;
+      tail.prev = next;
+    }else{
+      n.prev = tail;
+      tail.next = n;
+      tail = tail.next;
+      tail.next = head;
+      head.prev = tail;
+    }
+  }
+  return head;
+}
+
+let current;
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  const resultsList = makeList(resultCards);
+  console.log(resultsList)
+  current = resultsList;
+  current.val.style.display = 'block';
+  current.val.style.opacity = 100;
+})
+
+prevBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (!current.prev) return;
+  console.log({current});
+  current.val.style.opacity = 0;
+  setTimeout(() => {
+    current.val.style.display = 'none'
+    setTimeout(() =>{
+      current = current.prev;
+      current.val.style.display = 'block';
+      current.val.style.opacity = 100;
+    })
+  }, 300);
+})
+
+nextBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (!current.next) return;
+  console.log({current});
+  current.val.style.opacity = 0;
+  setTimeout(() => {
+    current.val.style.display = 'none'
+    setTimeout(() =>{
+      current = current.next;
+      current.val.style.display = 'block';
+      current.val.style.opacity = 100;
+    })
+  }, 300);
+})
 
 document.addEventListener('submit', async (e) => {
   if (e.target.classList.contains('save-answer')){
