c809ff7c5cc6baeb5a5e42c9e12cc934f411a490
kyle <nojaim.k@gmail.com>
2025-12-21 03:23:46 -0500

    opening connection for fly

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use("/questions", questionsRoutes);
 // test configuration (/test/pw-auth/login-reg)
 app.use("/test", testConfig);
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
   console.log("server successfully running!");
 });
 
