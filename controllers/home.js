d44bace21b6d6f84eb7747d37ef4bf175f3068b5
kyle <nojaim.k@gmail.com>
2026-04-17 21:00:47 -0400

    pass whether or not its a new session and the user status

--- controllers/home.js
+++ controllers/home.js
@@ -2,138 +2,170 @@ import models from "../models/index.js";
 const { User, Category, Question, PracticeSession } = models;
 import s3client from "../controllers/aws.js";
 
-import {marked} from 'marked';
-import createDOMPurify from 'dompurify';
-import {JSDOM} from 'jsdom';
+import { marked } from "marked";
+import createDOMPurify from "dompurify";
+import { JSDOM } from "jsdom";
 
 // good chance this will be encompassed elsewhere
 export default {
   getHome: async (req, res) => {
-    try{
-      let defaultCategories = await Category.find({isDefault: true});
-      if (req.session?.optOut) defaultCategories = defaultCategories.filter(c => !c.is100Devs);
-      const userCategories = req.user?.id ? await Category.find({userId:req.user.id}) : null;
-      const allCats = userCategories ? Array.from(new Set([...userCategories.map(c => c.description), ...defaultCategories.map(c => c.description)])) : defaultCategories.map(c => c.description);
-      
-      res.render("index", { allCats });
-    }catch(getIndexPageError){
-      console.log({getIndexPageError});
-      res.status(500).json({message:getIndexPageError.message});
+    try {
+      let defaultCategories = await Category.find({ isDefault: true });
+      if (req.session?.optOut)
+        defaultCategories = defaultCategories.filter((c) => !c.is100Devs);
+      const userCategories = req.user?.id
+        ? await Category.find({ userId: req.user.id })
+        : null;
+      const allCats = userCategories
+        ? Array.from(
+            new Set([
+              ...userCategories.map((c) => c.description),
+              ...defaultCategories.map((c) => c.description),
+            ]),
+          )
+        : defaultCategories.map((c) => c.description);
+      const session = req.session;
+      const newSession = req.session.cookie.newSession && !req.user;
+      const loggedIn = req.user ? true : false;
+      req.session.cookie.newSession = false;
+
+      res.render("index", { allCats, newSession, loggedIn });
+    } catch (getIndexPageError) {
+      console.log({ getIndexPageError });
+      res.status(500).json({ message: getIndexPageError.message });
     }
   },
 
-  getUserDash: async (req,res) => {
-    try{
+  getUserDash: async (req, res) => {
+    try {
       const user = await User.findById(req.user.id);
-      const questions = await Question.find({userId: req.user.id});
-      const sessions = await PracticeSession.find({userId: req.user.id});
-      return res.render('dashboard', {
+      const questions = await Question.find({ userId: req.user.id });
+      const sessions = await PracticeSession.find({ userId: req.user.id });
+      return res.render("dashboard", {
         user,
         questions,
         sessions,
       });
-    }catch(getUserDashError){
-      console.log({getUserDashError});
-      return res.status(400).json({message:getUserDashError.message});
+    } catch (getUserDashError) {
+      console.log({ getUserDashError });
+      return res.status(400).json({ message: getUserDashError.message });
     }
   },
 
-  saveResource: async (req,res) => {
-    try{
-      const {resource} = req.body;
-      const user = await User.findByIdAndUpdate(req.user.id,{
-        $addToSet: {resources: resource},
-      },{new:true});
-      return res.status(201).json({message: 'your resource has been saved'})
-    }catch(saveResourceError){
+  saveResource: async (req, res) => {
+    try {
+      const { resource } = req.body;
+      const user = await User.findByIdAndUpdate(
+        req.user.id,
+        {
+          $addToSet: { resources: resource },
+        },
+        { new: true },
+      );
+      return res.status(201).json({ message: "your resource has been saved" });
+    } catch (saveResourceError) {
       console.log(saveResourceError);
-      return res.status(400).json({message:saveResourceError.message});
+      return res.status(400).json({ message: saveResourceError.message });
     }
   },
-  
-  getSession: async(req,res) => {
-    try{
-      const {sessionId} = req.body;
-      const session = await PracticeSession.findById(sessionId).populate('questions');
-      const userQuestions = await Question.find({userId:req.user.id});
-      const parentIds = new Set(userQuestions.map(q => q.parentId ? q.parentId.toString() : null).filter(Boolean));
-      const questions = session.questions.map(q => {
-        if (parentIds.has(q._id.toString())){
-          const updated = userQuestions.find(uq => {
-            return uq.parentId?.toString() === q._id.toString()
-          })
+
+  getSession: async (req, res) => {
+    try {
+      const { sessionId } = req.body;
+      const session =
+        await PracticeSession.findById(sessionId).populate("questions");
+      const userQuestions = await Question.find({ userId: req.user.id });
+      const parentIds = new Set(
+        userQuestions
+          .map((q) => (q.parentId ? q.parentId.toString() : null))
+          .filter(Boolean),
+      );
+      const questions = session.questions.map((q) => {
+        if (parentIds.has(q._id.toString())) {
+          const updated = userQuestions.find((uq) => {
+            return uq.parentId?.toString() === q._id.toString();
+          });
           return updated;
         }
         return q;
       });
-      const feedback = session.aiResponse ? session.aiResponse.questionResponse.map(res => {      
-        const window = new JSDOM('').window;
-        const pure = createDOMPurify(window);
-        const purified = pure.sanitize(marked.parse(res.feedback, {breaks:true}));
+      const feedback = session.aiResponse
+        ? session.aiResponse.questionResponse.map((res) => {
+            const window = new JSDOM("").window;
+            const pure = createDOMPurify(window);
+            const purified = pure.sanitize(
+              marked.parse(res.feedback, { breaks: true }),
+            );
 
-        return purified.replaceAll('\n', '<br>')
-                       .replaceAll('\\n', '<br>')
-                       .replaceAll('-', '<br>')
-                       .replaceAll('&lt;', '<br>')
-                       .replaceAll('&gt;', '<br>')
-                       .replaceAll('&nbsp;', '<br>')
-                       .replaceAll(',,,', '<br><br><br>')
-                       .replaceAll('###', '<br><br><br>');
-      }) : [];
+            return purified
+              .replaceAll("\n", "<br>")
+              .replaceAll("\\n", "<br>")
+              .replaceAll("-", "<br>")
+              .replaceAll("&lt;", "<br>")
+              .replaceAll("&gt;", "<br>")
+              .replaceAll("&nbsp;", "<br>")
+              .replaceAll(",,,", "<br><br><br>")
+              .replaceAll("###", "<br><br><br>");
+          })
+        : [];
       const audioKeys = await Promise.all(
-        session.audioKeys?.map(async key => key ? await s3client.getAudio(key) : key)
+        session.audioKeys?.map(async (key) =>
+          key ? await s3client.getAudio(key) : key,
+        ),
       );
 
-      return res.render('previousSession', {
+      return res.render("previousSession", {
         session,
         questions,
         feedback,
         audioKeys,
-      })
-    }catch(getSessionError){
-      console.log({getSessionError});
-      return res.status(400).json({message: getSessionError.message});
+      });
+    } catch (getSessionError) {
+      console.log({ getSessionError });
+      return res.status(400).json({ message: getSessionError.message });
     }
   },
 
-  deleteResource: async(req,res) =>{
-    try{
-      const {resource} = req.body;
+  deleteResource: async (req, res) => {
+    try {
+      const { resource } = req.body;
       const user = await User.findByIdAndUpdate(
         req.user.id,
         {
-          $pull: {resources: resource},
+          $pull: { resources: resource },
         },
-        {new:true}
-      )
-      return res.status(200).json({message:"successfully removed resource"})
-    }catch(deleteResourceError){
-      console.log({deleteResourceError});
-      return res.status(404).json({message: deleteResourceError.message})
+        { new: true },
+      );
+      return res.status(200).json({ message: "successfully removed resource" });
+    } catch (deleteResourceError) {
+      console.log({ deleteResourceError });
+      return res.status(404).json({ message: deleteResourceError.message });
     }
   },
 
-  updateUser: async(req,res) => {
-    try{
-      const {title,level,optOut} = req.body;
-      console.log({title,level,optOut})
+  updateUser: async (req, res) => {
+    try {
+      const { title, level, optOut } = req.body;
+      console.log({ title, level, optOut });
       const user = await User.findByIdAndUpdate(
         req.user.id,
         {
           $set: {
             [`info.title`]: title,
             [`info.level`]: level,
-            optOut : optOut || false,
+            optOut: optOut || false,
           },
         },
-        {new:true}
+        { new: true },
       );
       req.session.optOut = optOut;
       await req.session.save();
-      return res.status(200).json({message: "successfully updated user info"})
-    }catch(updateUserError){
-      console.log({updateUserError});
-      return res.status(400).json({message: updateUserError.message});
+      return res
+        .status(200)
+        .json({ message: "successfully updated user info" });
+    } catch (updateUserError) {
+      console.log({ updateUserError });
+      return res.status(400).json({ message: updateUserError.message });
     }
   },
 };
