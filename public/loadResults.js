7232f1f73925d7d49de181f012af626ea1347376
kyle <nojaim.k@gmail.com>
2026-09-20 22:09:50 -0400

    changed how loaded results are tracked, allow more time for more complex functionality

--- public/loadResults.js
+++ public/loadResults.js
@@ -1,42 +1,43 @@
-const waitText = document.getElementById('wait-text');
-document.addEventListener('DOMContentLoaded', async () => {
-  const sessionId = document.getElementById('sessionId').value;
+const waitText = document.getElementById("wait-text");
+document.addEventListener("DOMContentLoaded", async () => {
+  const sessionId = document.getElementById("sessionId").value;
   let count = 0;
-  
+
   const checkInterval = setInterval(async () => {
     try {
-      const response = await fetch('/practice/checkSession', {
+      const response = await fetch("/practice/checkSession", {
         method: "POST",
-        headers: {"Content-Type" : "application/json"},
-        body:JSON.stringify({sessionId}),
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ sessionId }),
       });
-      if (response.ok && response.status !== 206){
+      if (response.ok && response.status !== 206) {
         const data = await response.json();
         clearInterval(checkInterval);
         window.location.assign(`/practice/${data.sessionId}`);
-      }else {
+      } else {
         count++;
-        waitText.textContent = count >= 15
-                                ? `this has never happened before`
-                              :count >= 12
-                                ? `ok tf ?!?!?!`
-                              :count >=  9
-                                ? `i swear it's me and not you`
-                              :count >=  6
-                                ? `don't read into this`
-                              :count >=  3
-                                ? `still cooking...`
-                                : waitText.textContent;
-        if (count >= 18){
-          waitText.textContent = 'sorry, there may have been an error, well look into this. theres a good chance you can still access this feedback from the previousSessions page or your dashboard';
+        waitText.textContent =
+          count >= 25
+            ? `This has never happened before (don't worry, it actually has).`
+            : count >= 20
+              ? `ok tf ?!?!?!`
+              : count >= 15
+                ? `I swear it's me and not you`
+                : count >= 10
+                  ? `Don't read into this`
+                  : count >= 5
+                    ? `Still cooking...`
+                    : waitText.textContent;
+        if (count >= 30) {
+          waitText.textContent = `There's a good chance that the model is just replying slowly and you can still access this feedback from the "Previous Sessions" page or your dashboard. If there is an error, my bad, I'll look into it!`;
           clearInterval(checkInterval);
-          setTimeout(() => window.location.replace('/dashboard'),3000);
+          setTimeout(() => window.location.replace("/dashboard"), 7000);
         }
       }
-    }catch(error){
-      console.log({error});
+    } catch (error) {
+      console.log({ error });
       count++;
-      if (count >= 15) clearInterval(checkInterval);
+      if (count >= 28) clearInterval(checkInterval);
     }
-  },2000)
-})
+  }, 1000);
+});
